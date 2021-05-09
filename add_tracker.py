891339d10a0fa666c119fae350713ffa8bec311f
Kcorb0 <68121651+Kcorb0@users.noreply.github.com>
2021-05-09 23:49:37 +0100

    Added class for creating new trackers

--- add_tracker.py
+++ add_tracker.py
@@ -1,2 +1,10 @@
 import tkinter as tk
 from tkinter import ttk
+
+
+class Tracker:
+    def __init__(self, name, start_time, start_date, target_date):
+        name = name
+        start_time = start_time
+        start_date = start_date
+        target_date = target_date
